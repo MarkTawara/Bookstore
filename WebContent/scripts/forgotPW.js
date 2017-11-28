c4fc53173c66bed9cc07733da9b7af8ced6ac353
Mark-Laptop\Mark <mhtawara@gmail.com>
2017-11-27 22:22:14 -0500

    Create js for forgotten password

---
+++ WebContent/scripts/forgotPW.js
@@ -0,0 +1,23 @@
+//Function handles error checking when inputting an email to the forgot email page
+$(document).ready(function(){
+	$('#forgotpw').submit(function(submission){
+
+		//Send an alert to the user if one or more fields is left blank.
+		//Loop controls the alert to just one alert, instead of alerting the user for all the fields left blank.
+		var loop = true;
+		$(':input').each(function(){
+			if($(this).val()==="" && loop===true){
+				alert("One or more fields have been left blank.");
+				loop = false;
+			}
+		});
+		
+		var email = document.getElementById("email").value;
+		if(email.indexOf("@") == -1){
+			alert("Please use a valid form of email address.");
+		}else if(email.indexOf(".") == -1){
+			alert("Please use a valid form of email address.");
+		}
+
+	});
+});
