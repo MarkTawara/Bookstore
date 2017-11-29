d431db0151305ff5958c5da4a7d08dbc35ed1b7f
Mark <mhtawara@gmail.com>
2017-11-28 22:50:38 -0500

    Cleaned up code, added comments

--- src/boundary/Login_Servlet.java
+++ src/boundary/Login_Servlet.java
@@ -21,97 +21,77 @@ public class Login_Servlet extends HttpServlet {
 	String globalEmail = "";
 
 	/**
-	 * @see HttpServlet#HttpServlet()
+	 * Constructor
 	 */
 	public Login_Servlet() {
 		super();
 	}
 
+	
+	/**
+	 * Verify the login credentials. Send user to index.jsp on success, and ___ on failure
+	 */
 	public void verify_login(HttpServletRequest request, HttpServletResponse response) throws SQLException {
+		//Get variables from user
 		String email = request.getParameter("reg_email");
 		String password = request.getParameter("reg_password");
+		
+		//Check if there is already a session running. If there is, terminate it
 		HttpSession session = request.getSession(false);
 		if (session != null) {
 			session.invalidate();
 		}
 		
+		//The next forwarded page
+		String nextJSP = "/index.jsp";
 		
+		//check if the user exists and allow the user to log in
 		bookstore_query query = new bookstore_query();
-		
 		boolean checkLogin = query.validate_user(request, response, email, password);
+		
 		if (checkLogin) {
+			//Track the session based on the email provided
 			session = request.getSession();
 			session.setAttribute("email", email);
 			
+			//Get the name of the user from the database
 			String name = query.get_name(request, response, email);
 			session.setAttribute("name", name);
-			System.out.println("Fuck This " + name);
 			
-			String nextJSP = "/index.jsp";
+			//Send the user to the homepage upon login
 			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJSP);
 			try {
 				System.out.println("Entered");
 				dispatcher.forward(request,response);
 			} catch (ServletException e1) {
 				System.out.println("Entered2");
-				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			} catch (IOException e1) {
 				System.out.println("Entered3");
-				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-			
-			try {
-				response.sendRedirect("index.jsp");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			/*
-			try {
-				request.getRequestDispatcher("index.jsp").forward(request, response);
-			} catch (ServletException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			*/
 		} else {
-			System.out.println("Still Fuck This");
-		}
-		
-		/*
-		if (bookstore_query.validate_user(request, response, email, password)) {
+			//The user provided the wrong password.
+			nextJSP = "login.jsp";
 			
-		}*/
+		}
 	}
 	
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * Go to verify login
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-			System.out.println("hello");
+			System.out.println("Reached doGet of Login_Servlet");
 			try {
 				verify_login(request, response);
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			/*
-			catch (SQLException e) {
-				e.printStackTrace();
-			}
-			 */
 	}
 	
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * Go to doGet
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
